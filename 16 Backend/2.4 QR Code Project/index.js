b4f76ca094f68e1d50884e50bd53c340d9736d6c
Kojo Anyane Obese <134105909+0bese@users.noreply.github.com>
2024-02-14 10:37:55 -0500

    Made a Qr-code generator which takes user url inputs then creates a png image of the QRcode

---
+++ 16 Backend/2.4 QR Code Project/index.js
@@ -0,0 +1,31 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+    message : "Enter the url: ",
+    name : "url",
+    },
+    ])
+  .then((answers) => {
+    const URL = answers.url
+    var qr_img = qr.image(URL, { type: 'png' });
+    qr_img.pipe(fs.createWriteStream('qr-image.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      throw new Error("Error using inquirer")
+    } else {
+      // Something else went wrong
+    }
+});
+
+
+  
