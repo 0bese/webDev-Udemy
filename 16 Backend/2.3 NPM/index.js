e4adc749a5a3b08e685038d3f491ab0adf2053cc
Kojo Anyane Obese <134105909+0bese@users.noreply.github.com>
2024-02-14 04:47:36 -0500

    installing, importing and using the packages

---
+++ 16 Backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+const superHeroName = superheroes.random();
+
+console.log(`I am ${superHeroName}!`);
